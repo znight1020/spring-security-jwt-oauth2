b8c61794862e7a004f4d64ef352d2e0305b02250
이현수 <znight1020@naver.com>
2024-07-13 22:27:39 +0900

    feat: SecurityConfig - oauth2 설정

--- auth/src/main/java/training/ex/config/SecurityConfig.java
+++ auth/src/main/java/training/ex/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package training.ex.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
@@ -15,7 +17,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((req) -> req
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
@@ -25,6 +29,10 @@ public class SecurityConfig {
                         configurer.loginPage("/loginForm")
                                 .loginProcessingUrl("/login")
                                 .defaultSuccessUrl("/")
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
 
         return http.build();
