29e9719c3887b3117b712c964eb6c18e4fbd1ed2
이현수 <znight1020@naver.com>
2024-07-13 22:44:12 +0900

    feat: User Entity 추가

---
+++ auth/src/main/java/training/ex/dto/User.java
@@ -0,0 +1,10 @@
+package training.ex.dto;
+
+public class User {
+    private String userUuid;
+    private String userEmail;
+    private String userPassword;
+    private String userName;
+    private String userStatus;
+    private String socialProvider;
+}
