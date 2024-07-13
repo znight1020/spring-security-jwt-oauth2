e2b1e0c0f4e9542597e810ccda5b2431df331583
이현수 <znight1020@naver.com>
2024-07-13 21:42:23 +0900

    design: 사용자 로그인 폼 생성

---
+++ auth/src/main/java/training/ex/controller/AuthController.java
@@ -0,0 +1,13 @@
+package training.ex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("loginForm")
+    public String loginForm() {
+        return "loginForm.html";
+    }
+}
